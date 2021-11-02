cc2cc57dffabb1939ed9a9a58d6110f083dfca53
Leonela9425 <leitogris@gmail.com>
2021-11-02 11:21:37 -0500

    test

--- Evaluación.R
+++ Evaluación.R
@@ -1,3 +1,35 @@
-install.packages("data.table")
-library("data.table")
+
+library(readxl)
+Educacion_04 <- read_excel("C:/Users/Edwin/Downloads/Educacion_04.xlsx")
+View(Educacion_04)
+
+library(readxl)
+Educacion_03 <- read_excel("C:/Users/Edwin/Downloads/Educacion_03.xlsx")
+View(Educacion_03)
+
+library(readxl)
+Educacion_01 <- read_excel("C:/Users/Edwin/Downloads/Educacion_01.xlsx")
+View(Educacion_01)
+
+library(xlsx)
+educacion1 <- read.xlsx("C:/Users/Edwin/Downloads/Educacion_04.xlsx", header= TRUE, rowIndex=3:37, sheetIndex=1)
+print(educacion1)
+
+educacion2 <- read.xlsx("C:/Users/Edwin/Downloads/Educacion_03.xlsx", header= TRUE, rowIndex=3:37, sheetIndex=1)
+print(educacion2)
+
+educacion3 <- read.xlsx("C:/Users/Edwin/Downloads/Educacion_01.xlsx", header= TRUE, rowIndex=3:37, sheetIndex=1)
+print(educacion3)
+
+Educacion_04 <- c("Educacion_04")
+Educacion_04 <- Educacion_04$Postrgado %in% c(3,37)
+head(Educacion_04)
+
+library(plyr)
+head(join(educacion.3, educacion.2, educacion.1, by="Entidad.federativa"))
+educ.list <- list(educacion.1, educacion.2, educacion.3)
+head(join_all(educ.list, by = "Entidad.federativa"))
+combine <- full_join(Educacion_04, Education_3, Educacion_01, by = "Entidad federativa") %>%
+   select("Entidad federativa, Sin escolaridad, Preescolar, Primaria, Secundaria, Preparatoria o bachillerato, Licenciatura o equivalente, Posgrado, Asiste, No asiste, Sabe leer y escribi, No sabe leer y escribir")
+  View(combine)
 
